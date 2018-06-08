9ce527b4537af8c7e05cd2f3d4d4fb8f1e70ff5f
高辛格 <xg.gao@tianrang-inc.com>
2018-06-08 22:48:51 +0800

    add util and token

---
+++ src/main/java/calculator/Tokenizer/Tokenizer.java
@@ -0,0 +1,4 @@
+package calculator.Tokenizer;
+
+public class Tokenizer {
+}
---
+++ src/main/java/calculator/util/Pair.java
@@ -0,0 +1,12 @@
+package calculator.util;
+
+public class Pair<K, V> {
+    private K key;
+    private V value;
+
+    public K getKey() { return key; }
+    public void setKey(K key) { this.key = key; }
+
+    public V getValue() { return value; }
+    public void setValue(V value) { this.value = value; }
+}
